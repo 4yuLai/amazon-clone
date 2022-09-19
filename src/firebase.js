6bf05189f373d48663304691430dd5f98261b260
4yuLai <siyu.lai.valerie@gmail.com>
2022-09-19 18:54:35 +0100

    initiate

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDqzAgHIezFa977YgF1zG61T9hNDl82Ryg",
+    authDomain: "clone-7756a.firebaseapp.com",
+    databaseURL: "https://clone-7756a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "clone-7756a",
+    storageBucket: "clone-7756a.appspot.com",
+    messagingSenderId: "215544905337",
+    appId: "1:215544905337:web:a873c3ee418ec8a8c4dea8",
+    measurementId: "G-MH6YCXJ4TQ"
+  };
