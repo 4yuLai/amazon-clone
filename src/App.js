eaf2e6b62a956d6e0c7bfb958921fdd9d4907f08
4yuLai <siyu.lai.valerie@gmail.com>
2022-09-27 00:43:49 +0100

    signin/out & connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
@@ -6,8 +6,36 @@ import Checkout from './Checkout';
 import Login from "./Login";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useStateValue } from "./StateProvider";
+
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(()=> {
+    //will only run once when the app component loads
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      console.log('THE USER IS>>> ', user);
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+        // ...
+      } else {
+        // User is signed out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  },[])
+
   return (
     <Router>
       <div className="app">
