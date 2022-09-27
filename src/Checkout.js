c98844583348663d5fc0c986f2eb8ea48ec1f281
4yuLai <siyu.lai.valerie@gmail.com>
2022-09-27 01:05:14 +0100

    firebase deploy

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,13 +5,14 @@ import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
     return (
         <div className='checkout'>
             <div className='checkout__left'>
                 <img className='checkout__ad' src='https://images-eu.ssl-images-amazon.com/images/G/02/giftcard/barbwehr/uk_q222_evergreen/xcm_banners_gc_phone_650x45-bw3yq_1300x90_gb-en._CB626501920_.jpg' alt='' />
                 
                 <div>
+                    <h3>Hello, {user?.email}</h3>
                     <h2 className='checkout__title'>Your Shopping Basket</h2>
                     {basket.map(item => (
                         <CheckoutProduct 
